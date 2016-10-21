b770c7fb36e6162296865e57247f8c63cf06f829
Johnisgeek <johnisgeek@gmail.com>
2016-10-21 15:39:29 +0200

    Update tyler.py

--- tyler.py
+++ tyler.py
@@ -1,5 +1,7 @@
+
 __author__ = 'jadams'
 
+import numpy as np
 import nltk
 import os
 import re
@@ -10,45 +12,46 @@ import csv
 import simplejson as json
 import paramiko
 
+
 class auto(object):
-	
-	
+    def __init__(self):
+        self.tylersList = []
+        print('Welcome. Lets Automate! \nWe will now start tasks...')
 
-        def __init__(self):
-                print('Welcome. Lets Automate! \nWe will now start tasks...')
-	
-#CSV Creation
-        def creation(self):
-                print('opening tyler.csv...')
-                self.csv = open('tyler.csv', 'rb')
-                self.csvRead = csv.reader(self.csv, delimiter=',')
-                auto.tylersList = list(self.csvRead)
-                print(self.tylersList)
-		auto.tylersList = auto.tylersList.split()
-                print(type(at.tylersList))
-           
-	def connection(self):
-                self.X = ('')
-		self.ip = at.tylersList[0]
-		print(self.ip)
-		self.user = at.tylersList[1]
-                self.password = at.tylersList[2]
-		self.info = []
-                print('Opening SSH Connection to tyler.csv Definitions...')
-                self.ssh = paramiko.SSHClient()
-                self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                self.ssh.connect(ip, username=self.user, password=self.password)
-                stdin , stdout , stderr = ssh.exec_command()
-                for line in stdout:
-                        self.X = (line.strip())
-                        self.info.append(self.X)
-                        print(self.X)
-                        print('Completed!')
-                        print('Closing Connection')
-                        self.ssh.close()
-                        print('Enjoy your day')
+    # CSV Creation
+    def creation(self):
+        print('opening tyler.csv...')
+        self.csv = open('tyler.csv', 'rb')
+        self.csvRead = csv.reader(self.csv)
+        self.tylersListPrep = list(self.csvRead)
+        print(self.tylersListPrep)
+        self.tylersList = np.asarray(self.tylersListPrep)
+        print(type(self.tylersList))
+        print(self.tylersList)
 
-auto.tylersList = []
+    def connection(self):
+        self.X = ('')
+        self.ip = at.tylersList[0,0]
+        print(self.ip)
+        self.user = at.tylersList[0,1]
+        self.password = at.tylersList[0,2]
+        self.info = []
+        self.command = at.tylersList[0,3]
+        print('Opening SSH Connection to tyler.csv Definitions...')
+        self.ssh = paramiko.SSHClient()
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.ssh.connect(self.ip, username=self.user, password=self.password)
+        stdin, stdout, stderr = self.ssh.exec_command(self.command)
+        for line in stdout:
+            self.X = (line.strip())
+            self.info.append(self.X)
+            print(self.X)
+            print('Completed!')
+            print('Closing Connection')
+            self.ssh.close()
+            print('Enjoy your day')
+
+
 at = auto()
 at.creation()
 at.connection()
